17547a3b128e44b5777064abb1f092266e48b870
xanorbkid <corps.bronax@gmail.com>
2025-06-16 12:03:24 +0000

    added views

--- website/views.py
+++ website/views.py
@@ -4,3 +4,25 @@ from django.shortcuts import render
 def home(request):
 
     return render(request, "home.html")
+
+def about(request):
+
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def services(request):
+    return render(request, "services.html")
+
+def blog(request):
+    return render(request, "blog.html")
+
+def blog_detail(request, post_id):
+    # In a real application, you would fetch the blog post from the database using post_id
+    # For now, we will just render a placeholder template
+    return render(request, "blog_detail.html", {"post_id": post_id})
+
+def faq(request):
+    return render(request, "faq.html")
+
