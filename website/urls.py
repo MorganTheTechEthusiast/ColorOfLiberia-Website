c56452d49f41c108562111c8dd6444dd3d547dd1
xanorbkid <corps.bronax@gmail.com>
2025-06-18 18:38:44 +0000

    updated business details

--- website/urls.py
+++ website/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", home, name="home"),
     path("about/", about, name="about"),
     path("explore/", explore, name="explore"),
-    path("business_details/", business_details, name="business_details"),
+    path("business_details/<int:business_id>", business_details, name="business_details"),
     path("contact/", contact, name="contact"),
     path("blog/", blog, name="blog"),
      
